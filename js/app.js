f7c9f3a8cec5308210dc7b95da8ddefad67686aa
jesusrojas-frontend <jesusrojas.frontend@gmail.com>
2017-10-11 15:52:44 -0400

    Se agrego la informacion a la pagina de preguntas-frecuentes y se agrego el logo al header

--- js/app.js
+++ js/app.js
@@ -1,15 +1,11 @@
-var ser = $('.servicio'), tr= $('.trabajamos'), qu= $('.quienes'), serv = $('.servicios'), numero = 1, tamano;
+
 $(window).scroll(function() {
 	//Servicios
-	tr.each(function(){
+	$('.pregunta').each(function(){
 	    var imagePos = $(this).offset().top;
 	    var topOfWindow = $(window).scrollTop();
-		if (imagePos <= topOfWindow+200) {
-			$('.trabajamos .como .numerob').css({
-				"visibility": "none",
-				"animation-delay": ".3s"
-			})
-			$('.trabajamos .como li').addClass("stretchLeft");
+		if (imagePos <= topOfWindow) {
+			
 		}
 	});
 });
